f390ec017a45a05696dd5248f678a18ffc873b54
Jared S <jaredjames2020@gmail.com>
2019-12-17 22:49:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-require "./config/environment"
+# require "./config/environment"
 require "./app/models/user"
 class ApplicationController < Sinatra::Base
 
@@ -17,12 +17,13 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-		user = User.new(:username => params[:username], :password => params[:password])
+	  user = User.new(:username => params[:username], :password => params[:password])
+
 		if user.save
-      redirect "/login"
-    else
-      redirect "/failure"
-    end
+			redirect "/login"
+		else
+			redirect "/failure"
+		end
 	end
 
 	get "/login" do
@@ -30,13 +31,14 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/login" do
-		user = User.find_by(:username => params[:username])
-		if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      erb :success
-    else
-      redirect "/failure"
-    end
+	  user = User.find_by(:username => params[:username])
+
+	  if user && user.authenticate(params[:password])
+	    session[:user_id] = user.id
+	    redirect "/success"
+	  else
+	    redirect "/failure"
+	  end
 	end
 
 	get "/success" do
@@ -58,11 +60,11 @@ class ApplicationController < Sinatra::Base
 
 	helpers do
 		def logged_in?
-			!!session[:id]
+			!!session[:user_id]
 		end
 
 		def current_user
-			User.find(session[:id])
+			User.find(session[:user_id])
 		end
 	end
 
